de7285c1b1a84d65bbcc86fb4b01a06ce2221b83
robertbutacu <robertbutacu@gmail.com>
2018-05-28 00:24:46 +0300

    >> on Parser

--- src/main/scala/data/operations/ParserComposer.scala
+++ src/main/scala/data/operations/ParserComposer.scala
@@ -6,10 +6,13 @@ import scala.util.Try
 
 trait ParserComposer {
   def andThen(p: Parser): Try[String]
+  def >>(p: Parser): Try[String]
 }
 
 object ParserComposer {
-  implicit class ImplicitParserComposer(p: Parser) extends ParserComposer {
+  implicit class ImplicitParserComposer(parser: Parser) extends ParserComposer {
     override def andThen(p: Parser): Try[String] = ???
+
+    override def >>(p: Parser): Try[String] = this.>>(p)
   }
 }
