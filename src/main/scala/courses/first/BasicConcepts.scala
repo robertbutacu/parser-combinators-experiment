d7aba3da747327b2bcd322bade4c9c56e0b96061
robertbutacu <robertbutacu@gmail.com>
2018-05-27 18:38:23 +0300

    parseCharUncurried

--- src/main/scala/courses/first/BasicConcepts.scala
+++ src/main/scala/courses/first/BasicConcepts.scala
@@ -1,10 +1,23 @@
 package courses.first
 
 object BasicConcepts {
+  type Message = String
+
   def AParser(s: String): (Boolean, String) = {
     s.head.toLower match {
       case 'a' => (true, s.tail)
       case _ => (false, s)
+    }
+  }
+
+  def parseCharUncurried(s: String, c: Char): (Message, String) = {
+    if (s.isEmpty)
+      ("Parsed the whole string", "")
+    else {
+      s.head.toLower match {
+        case found if found == c.toLower => (s"""Found $c""", s.tail)
+        case _ => (s"""Expected $c, found ${s.head}""", s)
+      }
     }
   }
 }
--- src/test/scala/courses/first/BasicConceptsTests.scala
+++ src/test/scala/courses/first/BasicConceptsTests.scala
@@ -5,6 +5,13 @@ import org.scalatest.FlatSpec
 class BasicConceptsTests extends FlatSpec {
   lazy val goodString = "ABC"
   lazy val badString = "ZBC"
+  lazy val customLetter = 'A'
+
+  def injectCharIntoPositiveMessage(c: Char): String =
+    s"""Found $c"""
+
+  def injectCharIntoNegativeMessage(expected: Char, got: Char): String =
+    s"""Expected $expected, found $got"""
 
   "Give a string starting with A " should " parse the string" in {
     assert(BasicConcepts.AParser(goodString) === (true, "BC"))
@@ -14,4 +21,11 @@ class BasicConceptsTests extends FlatSpec {
     assert(BasicConcepts.AParser(badString) === (false, badString))
   }
 
+  "Given a string starting with a custom letter" should "parse the string" in {
+    assert(BasicConcepts.parseCharUncurried(goodString, customLetter) === (injectCharIntoPositiveMessage('A'), goodString.tail))
+  }
+
+  "Given a string not starting with a custom letter" should "not parse the string" in {
+    assert(BasicConcepts.parseCharUncurried(badString, customLetter) === (injectCharIntoNegativeMessage('A', 'Z'), badString))
+  }
 }
