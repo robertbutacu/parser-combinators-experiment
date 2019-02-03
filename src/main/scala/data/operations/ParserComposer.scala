85210f398b7945effd8a21d701e4483af369aa54
robertbutacu <17516693+robertbutacu@users.noreply.github.com>
2019-02-03 18:59:39 +0000

    anyOf and choice

--- src/main/scala/data/operations/ParserComposer.scala
+++ src/main/scala/data/operations/ParserComposer.scala
@@ -1,5 +1,6 @@
 package data.operations
 
+import courses.first.BasicConcepts
 import data.{Parser, Result}
 
 import scala.util.{Failure, Success, Try}
@@ -8,8 +9,8 @@ trait ParserComposer[A, B] {
   def andThen(p: Parser[A, B]): Parser[A, B]
   def >>(p: Parser[A, B]): Parser[A, B] // andThen sugar
   def orElse(p: Parser[A, B]): Parser[A, B]
-  def choice: Try[Result[A, B]]
-  def anyOf: Try[Result[A, B]]
+  def choice(otherParsers: List[Parser[A, B]]): Parser[A, B]
+  def anyOf(ls: List[A], f: B => Try[Result[A, B]]): Parser[A, B]
   def run(input: B): Try[Result[A, B]]
   def <|>(p: Parser[A, B]): Parser[A, B] // orElse sugar
   def map[C](f: A => C): Parser[C, B]
@@ -28,7 +29,7 @@ object ParserComposer {
       Parser(p.c, innerFnc)
     }
 
-    override def >>(p: Parser[A, B]): Parser[A, B] = this.andThen(p)
+    override def >>(p: Parser[A, B]): Parser[A, B]     = this.andThen(p)
 
     override def orElse(p: Parser[A, B]): Parser[A, B] = {
       def innerFnc(input: B): Try[Result[A, B]] = {
@@ -43,15 +44,15 @@ object ParserComposer {
       Parser(p.c, innerFnc)
     }
 
-    override def choice: Try[Result[A, B]] = ???
+    override def choice(otherParsers: List[Parser[A, B]]): Parser[A, B]      = (otherParsers :+ parser).reduce(_ <|> _)
 
-    override def anyOf: Try[Result[A, B]] = ???
+    override def anyOf(ls: List[A], f: B => Try[Result[A, B]]): Parser[A, B] = ls.map(a => Parser(a, f)).reduce(_ <|> _)
 
-    override def run(input: B): Try[Result[A, B]] = parser.s(input)
+    override def run(input: B): Try[Result[A, B]]   = parser.s(input)
 
     override def <|>(p: Parser[A, B]): Parser[A, B] = this.orElse(p)
 
-    override def map[C](f: A => C): Parser[C, B] = {
+    override def map[C](f: A => C): Parser[C, B]    = {
       def mapOverParser(input: B): Try[Result[C, B]] = {
         parser.run(input).map(r => r.copy(value = f(r.value)))
       }
